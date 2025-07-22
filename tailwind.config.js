adec065be5d184b4c2d276cbccb7d652c5213606
aleziuzzz123 <aichatsyapp@gmail.com>
2025-07-22 18:33:29 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-background': '#100C14',
+        'brand-container': '#1A1620',
+        'brand-accent': '#A3FF00', // Lime Green
+        'brand-accent-secondary': '#F9F871', // Cyber Yellow
+        'brand-text-primary': '#FFFFFF',
+        'brand-text-secondary': '#A8A2B3',
+      }
+    },
   },
   plugins: [],
 }
